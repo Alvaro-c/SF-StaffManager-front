7701409194bafbd35f49c7e543b0895f390d8e6e
Álvaro Cañas <alvarocanasboal@gmail.com>
2023-04-26 21:52:55 +0200

    Create project, clean boilerplate

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+    <HomeView />
+
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
